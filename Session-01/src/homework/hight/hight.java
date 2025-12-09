0480a16b834049d08150a680a2b7bae93b69dc91
AliGHanizade <mralighanizade@gmail.com>
2025-12-09 08:20:02 +0330

    using variable for print value

---
+++ Session-01/src/homework/hight/hight.java
@@ -0,0 +1,14 @@
+package homework.hight;
+
+import java.awt.event.HierarchyBoundsAdapter;
+
+public class hight {
+    public static void main(String[] args) {
+        int Height = 170;
+        System.out.println("1 : "+ Height);
+        Height = 180;
+        System.out.println("2 : "+ Height);
+
+
+    }
+}
