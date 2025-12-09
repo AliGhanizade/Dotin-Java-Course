06bfa9c84d763bb0cd67849d0147a0cebfd868ec
AliGHanizade <mralighanizade@gmail.com>
2025-12-09 08:21:50 +0330

    using float and print

---
+++ Session-01/src/homework/weather/weather.java
@@ -0,0 +1,12 @@
+package homework.weather;
+
+public class weather {
+    public static void main(String[] args) {
+
+        float temperature = 36.6f;
+
+        boolean is_rainy = false;
+
+        System.out.printf("temperature => %f\t is Rainy => %b",temperature,is_rainy);
+    }
+}
